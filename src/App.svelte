8eb3865b1c819ffddf1627db653be63001b89eb7
kesarsauce <kesarsauce@gmail.com>
2022-10-09 23:24:25 +0530

    add countdown and some ui changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
-  let death = {last_words: ""}
+  import Countdown from "./lib/Countdown.svelte"
+  let death = { last_words: "Guess the person who said these last words" };
   let guess = "";
   let won = false;
   let out = [];
@@ -7,33 +8,36 @@
   async function fetchAllDeaths() {
     won = false;
     let response = await fetch("https://www.breakingbadapi.com/api/deaths");
-    out = await response.json()
-    out = out.filter(filterDeaths)
+    out = await response.json();
+    out = out.filter(filterDeaths);
   }
 
   function getNewDeath() {
+    guess = "";
+    won = false;
     const random = Math.floor(Math.random() * out.length);
-    death = out[random]
+    death = out[random];
   }
 
-  function filterDeaths(death){
-    let invalidLastWords = ["", "unknown", "unknwon"]
-    if (invalidLastWords.includes(death.last_words.toLowerCase())){
-      return false
+  function filterDeaths(death) {
+    let invalidLastWords = ["", "unknown", "unknwon"];
+    if (invalidLastWords.includes(death.last_words.toLowerCase())) {
+      return false;
     }
-    if (death.death.toLowerCase() == "declan"){
-      return false
+    if (death.death.toLowerCase() == "declan") {
+      return false;
     }
 
-    return true
+    return true;
   }
 
-  function validateGuess(){
+  function validateGuess() {
     if (guess != undefined) {
-      if (guess.toLowerCase() == death.death.toLowerCase()){
-      won = true;
-      }
-      else if (guess.toLowerCase() == death.death.split(" ")[0].toLowerCase()){
+      if (guess.toLowerCase() == death.death.toLowerCase()) {
+        won = true;
+      } else if (
+        guess.toLowerCase() == death.death.split(" ")[0].toLowerCase()
+      ) {
         won = true;
       }
     }
@@ -42,12 +46,66 @@
   fetchAllDeaths();
 </script>
 
-<button on:click={getNewDeath}>New</button>
+<main>
+  <div class="top-bar"><span class="timer"><Countdown/></span></div>
+  <h1>{death.last_words}</h1>
 
-<h1>{death.last_words}</h1>
+  <div>
+    <button type="button" on:click={getNewDeath} class="btn btn-outline"
+      >New</button
+    >
+  </div>
 
-<input bind:value={guess} on:input={validateGuess}>
+  <div>
+    <input
+      type="search"
+      bind:value={guess}
+      on:input={validateGuess}
+      placeholder="Type here"
+      class="input input-bordered input-info w-full max-w-xs"
+      class:input-success={won}
+    />
+  </div>
+</main>
+
 
 {#if won == true}
   <h2>Correct</h2>
 {/if}
+
+<style>
+  main {
+    height: 100vh;
+    width: 100vh;
+    display: flex;
+    overflow: none;
+    flex-flow: column nowrap;
+    gap: 5%;
+  }
+
+  .top-bar {
+    width: 100vh;
+    display: flex;
+    justify-content: space-between;
+    flex: 0.125;
+  }
+
+  .timer {
+    flex: 1;
+  }
+
+  input {
+    border-radius: 10px;
+    border: 2px solid #d4d4d4;
+    font-size: large;
+    width: max-content;
+  }
+
+  h1 {
+    flex: 0.25 1 auto;
+    overflow: auto;
+    font-family: "Courier New", Courier, monospace;
+    font-weight: 500;
+    min-height: 250px;
+  }
+</style>
---
+++ src/lib/Countdown.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let seconds = 60;
+    setInterval(() => {
+    if (seconds > 0) seconds--;
+  }, 1000);
+</script>
+
+<div class="grid grid-flow-col gap-5 text-center auto-cols-max">
+    <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+      <span class="countdown font-mono text-5xl">
+        <span style="--value:{seconds};"></span>
+      </span>
+    </div>
+</div>
